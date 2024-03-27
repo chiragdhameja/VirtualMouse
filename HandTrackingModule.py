fa72688fb50476fd9518ec4725fd2516f1d98f76
chiragdhameja <chiragdhameja@gmail.com>
2024-03-26 22:29:59 -0400

    minor tweeks

--- HandTrackingModule.py
+++ HandTrackingModule.py
@@ -2,25 +2,28 @@ import cv2
 import mediapipe as mp
 import math
 
+gc_mode = 0
 
 class handDetector():
-    def __init__(self, mode=False, maxHands=2, detectionCon=0.3, trackCon=0.5):
+    def __init__(self, mode=False, maxHands=2, modelComplexity=0, detectionCon=0.5, trackCon=0.5):
         self.mode = mode
         self.maxHands = maxHands
+        self.modelComplex = modelComplexity
         self.detectionCon = detectionCon
         self.trackCon = trackCon
 
         self.mpHands = mp.solutions.hands
-        self.hands = self.mpHands.Hands(self.mode, self.maxHands,
+        self.hands = self.mpHands.Hands(self.mode, self.maxHands, self.modelComplex,
                                         self.detectionCon, self.trackCon)
         self.mpDraw = mp.solutions.drawing_utils
 
         self.tipIds = [4, 8, 12, 16, 20]
+        self.fingers = []
+        self.lmlist = []
 
-    def findHands(self, img, draw=True):
+    def findHands(self, img, draw=True, flipType = True):
         imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
         self.results = self.hands.process(imgRGB)
-        # print(results.multi_hand_landmarks)
 
         if self.results.multi_hand_landmarks:
             for handLms in self.results.multi_hand_landmarks:
@@ -46,7 +49,7 @@ class handDetector():
                 # print(id, cx, cy)
                 self.lmList.append([id, cx, cy])
                 if draw:
-                    if id == 8:  # or id == 12:
+                    if id == 8:
                         cv2.circle(img, (cx, cy), 10, (255, 0, 255), cv2.FILLED)
             xMin, xMax = min(xList), max(xList)
             yMin, yMax = min(yList), max(yList)
@@ -75,16 +78,12 @@ class handDetector():
                 fingers.append(0)
         return fingers
 
-    def findDistance(self, p1, p2, img, draw=True, r=15, t=3):
+    def findDistance(self, p1, p2, img):
         x1, y1 = self.lmList[p1][1:]
         x2, y2 = self.lmList[p2][1:]
         cx, cy, = (x1 + x2) // 2, (y1 + y2) // 2
 
-        # if draw:
-        #    cv2.line(img, (x1, y1), (x2, y2), (255, 0, 255), t)
-        #    cv2.circle(img, (x1, y1), r, (255, 0, 255), cv2.FILLED)
-        #    cv2.circle(img, (x2, y2), r, (255, 0, 255), cv2.FILLED)
-        #    cv2.circle(img, (cx, cy), r, (0, 0, 255), cv2.FILLED)
+
         length = math.hypot(x2 - x1, y2 - y1)
 
         return length, img, [x1, y1, x2, y2, cx, cy]
--- virtualMouse.py
+++ virtualMouse.py
@@ -1,19 +1,26 @@
 # if changing devices
 
-# python 3.7.9 or lower
-# add python 2 as a virtual environment
-# install mediapipe(V 0.8.8)
+# python 3.8
+# install mediapipe
 # install openCV-python
-# install autopy (need python 3.7.9 or lower)
+# install autopy
+# install pyautogui
+# install PyCaw
 
 
+# import math
 import cv2
 import time
 import numpy as np
 import autopy
 import HandTrackingModule as htm
-from time import sleep
+# from time import sleep
 import pyautogui
+# import speech_recognition as speech
+# from threading import *
+from ctypes import cast, POINTER
+from comtypes import CLSCTX_ALL
+from pycaw.pycaw import AudioUtilities, IAudioEndpointVolume
 pyautogui.FAILSAFE = False
 
 # Dimensions of camera window
@@ -30,20 +37,64 @@ cap.set(3, camW)
 cap.set(4, camH)
 
 scrW, scrH = autopy.screen.size()
-# print(scrW, scrH)
 
 # Pink box size
-frameR = 120
+frameH = 80
+frameH2 = 230
+frameW = 120
 
 # mouse motion smoothening variable
-smoothen = 6
+smoothen = 5
 
 plocX, plocY = 0, 0
 clocX, clocY = 0, 0
 
 detector = htm.handDetector(maxHands=1)
 
+lsingleClickFlag = False
+rsingleClickFlag = False
+dbsingleClickFlag = False
+
+mouseHoldFlag = False
+
+
+
+devices = AudioUtilities.GetSpeakers()
+interface = devices.Activate(IAudioEndpointVolume._iid_, CLSCTX_ALL, None)
+volume = cast(interface, POINTER(IAudioEndpointVolume))
+
+volRange = volume.GetVolumeRange()
+
+minVol = int(volRange[0])
+maxVol = int(volRange[1])
+
+# sr = speech.Recognizer()
+# print("Waiting for command...")
+
 while True:
+
+    # with speech.Microphone(device_index=2) as mic:
+    #     t1 = Thread(target=audio.start)
+    #
+    #     audio = sr.listen(mic)
+    #     query = sr.recognize_google(audio, language='eng-in')
+    #     wake_up = "proton"
+    #
+    #     if wake_up in query:
+    #         print("inside")
+    #         wakeAudio = sr.listen(mic)
+    #         command = sr.recognize_google(wakeAudio, language='eng-in')
+    #
+    #
+    #         if 'start' in command:
+    #             print(command)
+    #             gc_mode = True
+    #
+    #         elif 'stop' in command:
+    #             print(command)
+    #             gc_mode = False
+
+
     success, img = cap.read()
     img = detector.findHands(img)
     lmList, bbox = detector.findPosition(img)
@@ -53,33 +104,99 @@ while True:
         x1, y1 = lmList[8][1:]
         x2, y2 = lmList[12][1:]
 
-        fingers = detector.fingersUp()
-        # print(fingers)
+        #li = detector.findHands(img)[0]
 
-        cv2.rectangle(img, (frameR, frameR), (camW - frameR, camH - frameR), (255, 0, 255), 2)
+        m1, n1 = lmList[5][1:]
+        m2, n2 = lmList[17][1:]
 
-        # if fingers[1] == 1 and fingers[2] == 0:
 
-        x3 = np.interp(x1, (frameR, camW - frameR), (0, scrW))
-        y3 = np.interp(y1, (frameR, camH - frameR), (0, scrH))
+        size = (n2 - n1) ** 2 + (m2 - m1) ** 2
+
+
+        #print(size)
+
+        cv2.rectangle(img, (frameW, frameH), (camW - frameW, camH - frameH2), (255, 0, 255), 2)
+
+        x3 = np.interp(x1, (frameW, camW - frameW), (0, scrW))
+        y3 = np.interp(y1, (frameH, camH - frameH2), (0, scrH))
 
         # to smoothen mouse movement
         clocX = plocX + (x3 - plocX) / smoothen
         clocY = plocY + (y3 - plocY) / smoothen
 
         autopy.mouse.move(scrW - clocX, clocY)
+        #pyautogui.moveTo(scrW - clocX, clocY)
 
         plocX, plocY = clocX, clocY
 
-        # finding length between 2 points
-        length, img, _ = detector.findDistance(4, 5, img)
-        # print(length)
 
-        if length < 30:
-        #   autopy.mouse.click()
+
+        if size > 4500:
+            point = 30
+            lpoint = 35
+            distance = "< 1m"
+        elif size < 4500 and size > 2000:
+            point = 15
+            lpoint = 20
+            distance = ">= 1m"
+        elif size <= 2000:
+            point = 10
+            lpoint = 15
+            distance = "> 2m"
+
+        #print("distance is ", distance)
+
+        # finding length between 2 points
+        #for left-click
+        lclklength, img, _ = detector.findDistance(4, 12, img)
+
+        if lclklength < lpoint and lsingleClickFlag != True:
+            lsingleClickFlag = True
             pyautogui.click()
 
+        elif lclklength > lpoint:
+            lsingleClickFlag = False
 
+
+        #for right-click
+        rclklength, img, _ = detector.findDistance(4, 16, img)
+
+        if rclklength < point and rsingleClickFlag != True:
+            rsingleClickFlag = True
+            pyautogui.rightClick()
+
+        elif rclklength > point:
+            rsingleClickFlag = False
+
+        # for double-click
+        dbclklength, img, _ = detector.findDistance(4, 20, img)
+
+        if dbclklength < point and dbsingleClickFlag != True:
+            dbsingleClickFlag = True
+            pyautogui.doubleClick()
+
+        elif dbclklength > point:
+            dbsingleClickFlag = False
+
+        if lmList[8][2] > lmList[5][2] and lmList[12][2] > lmList[9][2] and lmList[16][2] > lmList[13][2] and lmList[20][2] > lmList[17][2] and lmList[4][2] < lmList[1][2]:
+
+            # hand range = 250, 80
+            # vol range = -64, 0
+
+            lmList[4][2]
+            handRange = [80, 250]
+            cvtVol = [maxVol, minVol]
+
+            volLength = lmList[4][2]
+            vol = np.interp(volLength, handRange, cvtVol)
+            #print(vol)
+            volume.SetMasterVolumeLevel(vol, None)
+            actVol = [0,100]
+            actVol1 = np.interp(vol, cvtVol, actVol)
+            #print("Volume is: ",actVol1)
+
+        # if lmList[8][2] > lmList[0][2]:
+        #     break
 
 
 
